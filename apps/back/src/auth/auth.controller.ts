2b16c184041e06ca282dcb4fed70b40331c54894
bapbodanbbang <bapbodanbbang@gmail.com>
2022-12-21 20:52:23 +0900

    Add: signup controller, service format

--- apps/back/src/auth/auth.controller.ts
+++ apps/back/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ){}
+
+    @Post("signup")
+    async signup(@Body() { loginId, password, name }: any) {
+        await this.authService.createUser(loginId, password, name);
+    }
+}
--- apps/back/src/auth/auth.service.ts
+++ apps/back/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor() { }
+
+    async createUser(loginId:string, password:string, name:string) {}
+}

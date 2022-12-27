f79fe7011912376769f23252d9a6bf4a78d79fcc
bapbodanbbang <bapbodanbbang@gmail.com>
2022-12-27 23:58:13 +0900

    Feat: MailModule

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -6,9 +6,24 @@ import { AuthModule } from './auth/auth.module';
 import { PaymentModule } from './payment/payment.module';
 import { CoinModule } from './coin/coin.module';
 import { DatabaseModule } from './database/database.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter'
 
 @Module({
-  imports: [UserModule, AuthModule, PaymentModule, CoinModule, DatabaseModule],
+  imports: [UserModule, AuthModule, PaymentModule, CoinModule, DatabaseModule,
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new EjsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })

6830f3f75647406f011872df3687531db87647b0
Taerim Lee <43867711+ttaerrim@users.noreply.github.com>
2022-12-30 10:41:04 +0900

    chore: default setting (#17)
    * chore: install package
    
    * docs: update docs
    
    root에서 package install
    
    * chore: set base url
    
    * modify: folder structure
    
    * chore: install sass
    
    * chore: set sass path
    
    * feat: add style test
    
    * chore: set vscode setting
    
    Co-authored-by: Jade <104347645+jade-redbrick@users.noreply.github.com>

--- apps/front/src/pages/index.tsx
+++ apps/front/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "ui";
 
+import styles from "styles/Home.module.scss";
+
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className={styles.test}>Web</h1>
       <Button />
     </div>
   );

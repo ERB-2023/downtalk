3306eb0ae2a81565629ae8917a65d6749d6ec44e
Jade <104347645+jade-redbrick@users.noreply.github.com>
2023-01-01 20:12:46 +0900

    chore: eslint airbnb import setting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,42 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    'next',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'turbo',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  parserOptions: {
+    project: ['tsconfig.json'],
+  },
+  settings: {
+    'import/resolver': {
+      node: {},
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+  },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'never',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/export': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 };

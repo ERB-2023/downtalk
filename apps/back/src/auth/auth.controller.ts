95fc59fa854458f4fcc80092eeb2a9e3165879ce
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-15 15:19:12 +0900

    Feat: Google Auth

--- apps/back/src/auth/auth.controller.ts
+++ apps/back/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {}
+}

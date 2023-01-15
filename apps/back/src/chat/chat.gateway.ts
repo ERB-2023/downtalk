7bd2ba877093283d871d949a5b30fae6c1ba006f
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-15 14:19:30 +0900

    Feat: ChatGateway

---
+++ apps/back/src/chat/chat.gateway.ts
@@ -0,0 +1,40 @@
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket, Namespace } from 'socket.io';
+
+@WebSocketGateway(81, { namespace: 'chat' })
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor() {}
+
+  private readonly logger = new Logger(ChatGateway.name);
+
+  @WebSocketServer() namespace: Namespace;
+
+  @SubscribeMessage('message')
+  handleEvent(@MessageBody() data: string): string {
+    return data;
+  }
+
+  handleDisconnect(client: Socket) {
+    // throw new Error('Method not implemented.');
+    console.log(client);
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+    // throw new Error('Method not implemented.');
+    console.log(client);
+  }
+  afterInit(server: any) {
+    // throw new Error('Method not implemented.');
+    this.logger.log('Chat Socket Server Init!!!');
+  }
+}

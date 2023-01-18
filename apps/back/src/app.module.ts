d901457148c078b4042b6388c496131c8692f4eb
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-18 22:54:35 +0900

    Feat: Api Formatter

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { ResponseFormatInterceptor } from './common/interceptor/response.interceptor';
 
 @Module({
   imports: [ChatModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseFormatInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ apps/back/src/common/interceptor/response.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponse } from '../interface/response.interface';
+
+@Injectable()
+export class ResponseFormatInterceptor<T>
+  implements NestInterceptor<T, ApiResponse<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: context.switchToHttp().getResponse().statusCode,
+        data: data,
+        message: data.message || '',
+      })),
+    );
+  }
+}

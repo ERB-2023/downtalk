85ca10ddd927c79da89407030f0517f55c7c9048
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-18 21:08:34 +0900

    Feat: DatabaseModule (mysql)

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ChatModule, AuthModule],
+  imports: [ChatModule, AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })

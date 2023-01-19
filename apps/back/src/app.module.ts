9937fcd75ec1b626ab347e52fa6088b117cf7805
homesSync <111725661+miin-jae-Kim@users.noreply.github.com>
2023-01-19 19:47:31 +0900

    Chore: Friend Module

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { UserModule } from './user/user.module';
+import { FriendModule } from './friend/friend.module';
 
 @Module({
-  imports: [ChatModule, AuthModule, UserModule],
+  imports: [ChatModule, AuthModule, UserModule, FriendModule],
   controllers: [AppController],
   providers: [AppService],
 })

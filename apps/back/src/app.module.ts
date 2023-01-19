bbce84d1ee34b48b4d94a17724637a2a6a958a5c
homesSync <111725661+miin-jae-Kim@users.noreply.github.com>
2023-01-19 19:40:48 +0900

    Chore: User Module

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ChatModule, AuthModule],
+  imports: [ChatModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

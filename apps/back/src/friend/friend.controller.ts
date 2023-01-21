6543544b10fc33b79edeb3ca9794a1a24a3555c1
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-21 11:06:24 +0900

    Feat: get friends router

--- apps/back/src/friend/friend.controller.ts
+++ apps/back/src/friend/friend.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Request,
+} from '@nestjs/common';
+import { Friend } from 'src/database/entity/friend.entity';
+import { FriendService } from './friend.service';
 
 @Controller('friends')
 export class FriendController {
-    constructor() {}
+  constructor(private readonly friendService: FriendService) {}
 
-    @Post()
-    async requestFriend() {}
+  @Post()
+  async requestFriend() {}
 
-    @Get()
-    async getFriends() {}
+  @Get()
+  async getFriends(
+    @Request() req: { user: { id: number } },
+    @Query('limit', new DefaultValuePipe(0), ParseIntPipe) limit: number,
+    @Query('offset', new DefaultValuePipe(10), ParseIntPipe) offset: number,
+  ): Promise<Friend[]> {
+    return this.friendService.findFriends(req.user.id, limit, offset);
+  }
 }

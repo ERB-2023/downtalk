a771b7bfaf29d2baa3ecb1d5e5808fb3d07e4cc3
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-21 11:06:17 +0900

    Feat: get friends

--- apps/back/src/friend/friend.module.ts
+++ apps/back/src/friend/friend.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FriendService } from './friend.service';
 import { FriendController } from './friend.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { friendProviders } from 'src/database/providers/friend.provider';
 
 @Module({
-  providers: [FriendService],
-  controllers: [FriendController]
+  imports: [DatabaseModule],
+  providers: [FriendService, ...friendProviders],
+  controllers: [FriendController],
 })
 export class FriendModule {}
--- apps/back/src/friend/friend.service.ts
+++ apps/back/src/friend/friend.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { FRIEND_STATUS } from 'src/common/enum/friend.enum';
+import { Friend } from 'src/database/entity/friend.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class FriendService {}
+export class FriendService {
+  constructor(
+    @Inject('FRIEND_REPOSITORY')
+    private readonly friendRepository: Repository<Friend>,
+  ) {}
+
+  async findFriends(
+    userId: number,
+    limit: number,
+    offset: number,
+  ): Promise<Friend[]> {
+    return this.friendRepository.find({
+      where: {
+        requestUser: { id: userId },
+        status: FRIEND_STATUS.FRIEND,
+      },
+      take: limit,
+      skip: offset,
+    });
+  }
+}

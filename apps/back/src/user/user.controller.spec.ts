7353be2d2dd3a1c0800f78d3dfbe58fc661d740a
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-21 10:30:37 +0900

    Test: API, serachUsers (controller, service)

--- apps/back/src/user/user.controller.spec.ts
+++ apps/back/src/user/user.controller.spec.ts
@@ -15,4 +15,12 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('GET /users?searchKey&limit&offset', () => {
+    it('정상 호출시, statusCode 200 전송', () => {});
+
+    it('정상 호출시, Response data Promise<Users> 전송', () => {});
+
+    it('searchKey, limit, offset 중 하나라도 없는 경우 statusCode 400 전송', () => {});
+  });
 });

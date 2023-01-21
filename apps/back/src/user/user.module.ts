9fa313aacea9af6aa17532967378c5bb37e1e134
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-21 10:26:21 +0900

    Feat: userService, searchUser

--- apps/back/src/user/user.module.ts
+++ apps/back/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { userProviders } from 'src/database/providers/user.provider';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController]
+  imports: [DatabaseModule],
+  providers: [UserService, ...userProviders],
+  controllers: [UserController],
 })
 export class UserModule {}

3f79405a3d6e9590dcda22c02d322c7e0c8feb69
ttaerrim <ltr0121@naver.com>
2023-01-22 22:17:10 +0900

    style: default responsive view

--- apps/front/src/pages/_app.tsx
+++ apps/front/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
 import "styles/global.scss";
+import styles from "styles/Home.module.scss";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
+function App({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/react-in-jsx-scope, react/jsx-props-no-spreading
+  return (
+    <div className={styles.container}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
 
 export default App;

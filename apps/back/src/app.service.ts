9100183add096a25397c9c0bdfcf3c23925fb23f
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-24 22:21:01 +0900

    Fix: appService, return error

--- apps/back/src/app.service.ts
+++ apps/back/src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    throw new Error('Not implemented');
     return 'Hello World!';
   }
 }

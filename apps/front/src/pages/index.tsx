e496205eb736f93e1b8bed8ffe5a3553287518ab
ChrisK007 <tinachristina0218@gmail.com>
2023-01-24 19:00:53 +0900

    remove duplicate button

--- apps/front/src/pages/index.tsx
+++ apps/front/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { GoogleLogin } from "@react-oauth/google";
 import { Button } from "ui";
 import styles from "styles/Home.module.scss";
 
@@ -6,14 +5,6 @@ export default function Web() {
   return (
     <div>
       <h1 className={styles.test}>Web</h1>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-      />
       <Button />
     </div>
   );

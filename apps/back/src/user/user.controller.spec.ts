f3438f91eaa304eded42135e5ff17b917a79b43c
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-24 22:20:38 +0900

    Modify: user TestingMoudle(controller,service)

--- apps/back/src/user/user.controller.spec.ts
+++ apps/back/src/user/user.controller.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from 'src/database/database.module';
+import { User } from 'src/database/entity/user.entity';
+import { userProviders } from 'src/database/providers/user.provider';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
       controllers: [UserController],
+      providers: [UserService, ...userProviders],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
@@ -17,9 +25,17 @@ describe('UserController', () => {
   });
 
   describe('GET /users?searchKey&limit&offset', () => {
-    it('정상 호출시, statusCode 200 전송', () => {});
+    it('정상 호출시, statusCode 200 전송', async () => {
+      return service.searchUsers('test', 10, 0).then((users) => {
+        expect(users.every((user) => user instanceof User)).toBeTruthy();
+      });
+    });
 
-    it('정상 호출시, Response data Promise<Users> 전송', () => {});
+    it('정상 호출시, Response data Promise<Users> 전송', () => {
+      return service.searchUsers('test', 10, 0).then((users) => {
+        expect(users.every((user) => user instanceof User)).toBeTruthy();
+      });
+    });
 
     it('searchKey, limit, offset 중 하나라도 없는 경우 statusCode 400 전송', () => {});
   });
--- apps/back/src/user/user.service.spec.ts
+++ apps/back/src/user/user.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from 'src/database/providers/user.provider';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
@@ -6,7 +8,8 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      imports: [DatabaseModule],
+      providers: [UserService, ...userProviders],
     }).compile();
 
     service = module.get<UserService>(UserService);

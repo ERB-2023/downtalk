9982490417dd31cadd9d0e9674cc6105d1cd3b8b
bapbodanbbang <bapbodanbbang@gmail.com>
2023-01-28 22:59:58 +0900

    Feat: create room, find rooms

--- apps/back/src/chat/chat.module.ts
+++ apps/back/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
 
 @Module({
   providers: [ChatService, ChatGateway],
+  controllers: [ChatController],
 })
 export class ChatModule {}
--- apps/back/src/chat/chat.service.ts
+++ apps/back/src/chat/chat.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ChatRoom } from 'src/database/entity/chat-room.entity';
+import { User } from 'src/database/entity/user.entity';
+import { In, Repository } from 'typeorm';
+import { CreateChattingRoomDto } from './dto/create-room.dto';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @Inject('CHATROOM_REPOSITORY')
+    private readonly chatRoomRepository: Repository<ChatRoom>,
+    @Inject('USER_REPOSITORY')
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findRooms(
+    userId: number,
+    limit: number,
+    offset: number,
+  ): Promise<ChatRoom[]> {
+    return this.chatRoomRepository.find({
+      where: {
+        users: { id: userId },
+      },
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  async createRoom(
+    hostId: number,
+    roomInfo: CreateChattingRoomDto,
+  ): Promise<ChatRoom> {
+    const userIds: number[] = roomInfo.friends.map((friend) => friend.userId);
+    userIds.push(hostId);
+
+    const users = await this.userRepository.find({
+      where: { id: In(userIds) },
+    });
+
+    const chatRoom = await this.chatRoomRepository.create({
+      name: roomInfo.name,
+      users: users,
+    });
+
+    await this.chatRoomRepository.save(chatRoom);
+
+    delete chatRoom['createdAt'];
+    delete chatRoom['deletedAt'];
+    delete chatRoom['users'];
+
+    return chatRoom;
+  }
+}

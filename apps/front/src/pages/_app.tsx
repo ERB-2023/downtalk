73a6d830757bb976d1064ce02a6b74cc9e575778
ChrisK007 <tinachristina0218@gmail.com>
2023-01-29 19:26:06 +0900

    rename: change import path

--- apps/front/src/pages/_app.tsx
+++ apps/front/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GOOGLE } from "utils/constant";
+import { GOOGLE } from "data/google";
 import PageHead from "components/PageHead";
 import "styles/global.scss";
 import styles from "styles/Home.module.scss";

9d89d5dceb86e2e2ff602c221162752a4fe60f79
zwoong <zwoong6867@gmail.com>
2023-02-12 00:45:17 +0900

    JwtStrategy-add

---
+++ apps/back/src/auth/auth.startegy.ts
@@ -0,0 +1,23 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      //Request에서 JWT 토큰을 추출하는 방법을 설정 -> Authorization에서 Bearer Token에 JWT 토큰을 담아 전송해야한다.
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      //true로 설정하면 Passport에 토큰 검증을 위임하지 않고 직접 검증, false는 Passport에 검증 위임
+      ignoreExpiration: false,
+      //검증 비밀 값(유출 주위)
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    const ci = payload.ci;
+    return await this.authService.findUser(ci);
+  }
+}

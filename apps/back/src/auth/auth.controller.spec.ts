49d54e7e79180fb8367c6fad39dda3f6ab809972
bapbodanbbang <bapbodanbbang@gmail.com>
2023-02-12 12:40:05 +0900

    Fix: auth test error

--- apps/back/src/auth/auth.controller.spec.ts
+++ apps/back/src/auth/auth.controller.spec.ts
@@ -1,15 +1,25 @@
+import { HttpModule } from '@nestjs/axios';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from 'src/database/providers/user.provider';
 import { AuthController } from './auth.controller';
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule, HttpModule, JwtModule],
+      providers: [AuthService, ...userProviders],
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {

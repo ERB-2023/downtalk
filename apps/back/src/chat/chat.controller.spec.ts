3e5d0988f2dfe3df6c39313c99cee4ca9e8f8973
bapbodanbbang <bapbodanbbang@gmail.com>
2023-02-12 12:41:48 +0900

    Fix: chat test error

--- apps/back/src/chat/chat.controller.spec.ts
+++ apps/back/src/chat/chat.controller.spec.ts
@@ -1,11 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from 'src/database/database.module';
+import { chatRoomProvider } from 'src/database/providers/chat-room.provider';
+import { userChatRoomProvider } from 'src/database/providers/user-chat-room.provider';
+import { userProviders } from 'src/database/providers/user.provider';
 import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
 
 describe('ChatController', () => {
   let controller: ChatController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
+      providers: [
+        ChatService,
+        ...userProviders,
+        chatRoomProvider,
+        userChatRoomProvider,
+      ],
       controllers: [ChatController],
     }).compile();
 

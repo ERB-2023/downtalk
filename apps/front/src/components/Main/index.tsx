608bfac6007fafe0808d4dd867a595b0f10f6b83
ttaerrim <ltr0121@naver.com>
2023-02-12 19:13:28 +0900

    refactor: main 내 폴더 분리

--- apps/front/src/components/Main/index.tsx
+++ apps/front/src/components/Main/index.tsx
@@ -2,16 +2,11 @@ import { useState } from "react";
 import Chat from "components/Chat";
 import DefaultHeader from "components/DefaultHeader";
 import Friends from "components/Friends";
+import Tab from "./components/Tab";
+import { CHAT, FRIEND, tabs } from "./data";
 import styles from "./index.module.scss";
-import Tab from "./Tab";
 
 function Main() {
-  const CHAT = "chat";
-  const FRIEND = "friend";
-  const tabs = [
-    { id: CHAT, label: "채팅방" },
-    { id: FRIEND, label: "친구" },
-  ];
   const [currentTab, setCurrentTab] = useState<string>(CHAT);
   const handleTab = (e: React.MouseEvent<HTMLInputElement>) => {
     setCurrentTab(e.currentTarget.id);

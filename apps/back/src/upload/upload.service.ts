11ba72542074fa7a3962838db409b5cc4ff07a2c
bapbodanbbang <bapbodanbbang@gmail.com>
2023-02-22 14:37:05 +0900

    Modify: use ConfigModule

--- apps/back/src/upload/upload.service.ts
+++ apps/back/src/upload/upload.service.ts
@@ -1,23 +1,29 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as AWS from 'aws-sdk';
 
-const s3 = new AWS.S3({
-  accessKeyId: 'AKIAU3URT645QPYANW5C',
-  secretAccessKey: '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',
-});
 @Injectable()
 export class UploadService {
-  uploadFile(userId: number, file: Express.Multer.File) {
-    //save to s3
-    //return s3 url
+  private s3: AWS.S3;
+
+  constructor(private configService: ConfigService) {
+    this.s3 = new AWS.S3({
+      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
+      secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+    });
+  }
+
+  async uploadFile(userId: number, file: Express.Multer.File): Promise<string> {
     try {
-      return s3
+      const result = await this.s3
         .upload({
-          Bucket: `downchat`,
+          Bucket: this.configService.get<string>('AWS_S3_BUCKET'),
           Key: `profile/${userId}${Date.now().toString()}-${file.originalname}`,
           Body: file.buffer,
         })
         .promise();
+
+      return result.Location;
     } catch (error) {
       throw new InternalServerErrorException();
     }

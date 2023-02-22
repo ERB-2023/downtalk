8caa26f3753fd21b6e682ca692ba9da3d9eb0f42
bapbodanbbang <bapbodanbbang@gmail.com>
2023-02-22 14:22:15 +0900

    Feat: upload profile

--- apps/back/src/upload/upload.service.ts
+++ apps/back/src/upload/upload.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 
+const s3 = new AWS.S3({
+  accessKeyId: 'AKIAU3URT645QPYANW5C',
+  secretAccessKey: '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',
+});
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  uploadFile(userId: number, file: Express.Multer.File) {
+    //save to s3
+    //return s3 url
+    try {
+      return s3
+        .upload({
+          Bucket: `downchat`,
+          Key: `profile/${userId}${Date.now().toString()}-${file.originalname}`,
+          Body: file.buffer,
+        })
+        .promise();
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}

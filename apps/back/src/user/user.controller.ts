8f21fae2301cff46393e8154f498218fa91284dd
bapbodanbbang <bapbodanbbang@gmail.com>
2023-03-05 17:02:17 +0900

    Feat: API, get my profile

--- apps/back/src/user/user.controller.ts
+++ apps/back/src/user/user.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserService } from './user.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -17,5 +27,11 @@ export class UserController {
   @Get('/:userId/profile')
   async getProfile(@Param('userId') userId: number) {
     return this.userService.getProfile(userId);
+  }
+
+  @Get('/profile')
+  async getMyProfile(@Req() req: { user: { id: number } }) {
+    console.log(req.user);
+    return this.userService.getProfile(req.user.id);
   }
 }

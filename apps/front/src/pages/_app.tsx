d6f33fe4dbbd745425e324eba564268e166e31bd
ChrisK007 <tinachristina0218@gmail.com>
2023-03-12 17:54:44 +0900

    set up redux

--- apps/front/src/pages/_app.tsx
+++ apps/front/src/pages/_app.tsx
@@ -1,19 +1,26 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GOOGLE } from "data/google";
 import PageHead from "components/PageHead";
+import UserInfo from "components/UserInfo";
 import "styles/global.scss";
 import styles from "styles/Home.module.scss";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <GoogleOAuthProvider clientId={GOOGLE.CLIENT_ID}>
-      <div className={styles.container}>
-        <PageHead />
-        <Component {...pageProps} />
-      </div>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={GOOGLE.CLIENT_ID}>
+        <UserInfo>
+          <div className={styles.container}>
+            <PageHead />
+            <Component {...pageProps} />
+          </div>
+        </UserInfo>
+      </GoogleOAuthProvider>
+    </Provider>
   );
 }
 

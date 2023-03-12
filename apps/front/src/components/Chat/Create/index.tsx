06e718188143f5415c7516c20214fe296b8f89e3
ttaerrim <ltr0121@naver.com>
2023-03-12 17:10:31 +0900

    comment: 주석 추가

--- apps/front/src/components/Chat/Create/index.tsx
+++ apps/front/src/components/Chat/Create/index.tsx
@@ -12,6 +12,7 @@ function Create() {
   return (
     <div className={styles.container}>
       <SectionHeader title="채팅방 만들기" onClickLeft={() => router.back()} />
+      {/* TODO: 섹션 헤더에 다음 버튼 추가하기 */}
       <div className={styles.wrapper}>
         <SearchBar
           value={searchMemberValue}
